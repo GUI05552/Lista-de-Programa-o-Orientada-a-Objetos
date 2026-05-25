e092ebb85c817d6ffd6a7f5c3ebbe12efa8fb0b2
GuilhermeMen <gui.mcastro11@gmail.com>
2026-05-25 08:46:18 -0400

    Delete 3_podcast.py

--- 3_podcast.py
+++
@@ -1,9 +0,0 @@
-from midia import Midia
-
-class Podcast(Midia):
-    def __init__(self, titulo, duracao, apresentador):
-        super().__init__(titulo, duracao)
-        self.apresentador = apresentador
-
-    def reproduzir(self):
-        print(f"Reproduzindo podcast '{self.titulo}' com {self.apresentador}")
